15af37bed119246eaa3afe52468f3da6867f2c9c
jonathan-arias <jarias17@ucla.edu>
2017-09-29 13:51:19 -0700

    Added primitive touch handler for grid cells

--- app/src/main/java/com/example/antoniocontreras/sudokusolver/GridView.java
+++ app/src/main/java/com/example/antoniocontreras/sudokusolver/GridView.java
@@ -6,14 +6,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import static android.content.ContentValues.TAG;
 
 public class GridView extends View {
     private int numColumns, numRows;
     private int cellWidth, cellHeight;
     private Paint black = new Paint();
     private Paint gray = new Paint();
+    private Paint blue = new Paint();
     private boolean[][] cellChecked;
 
     public GridView(Context context) {
@@ -22,6 +26,8 @@ public class GridView extends View {
         black.setStrokeWidth(8);
         gray.setColor(Color.DKGRAY);
         gray.setStrokeWidth(2);
+        blue.setColor(Color.BLUE);
+        blue.setTextSize(40);
     }
 
     public GridView(Context context, AttributeSet attrs) {
@@ -65,6 +71,7 @@ public class GridView extends View {
         invalidate();
     }
 
+    // See https://stackoverflow.com/questions/42280172/having-trouble-drawing-a-square-grid-for-an-android-game
     @Override
     protected void onDraw(Canvas canvas) {
         float startX;
@@ -82,6 +89,9 @@ public class GridView extends View {
         int xOffset = (width - boardSize) / 2;
         int yOffset = (height - boardSize) / 2;
 
+        String s = "1";
+        String t = "2";
+
         for (int i = 0; i <= gridSize; i++) {
             startX = xOffset + i*gridSpacing;
             startY = yOffset;
@@ -91,8 +101,10 @@ public class GridView extends View {
 
             if (i % 3 == 0){
                 canvas.drawLine(startX, startY, stopX, stopY, black);
+                //canvas.drawText(s, startX+10, startY+40, blue);
             } else {
                 canvas.drawLine(startX, startY, stopX, stopY, gray);
+                //canvas.drawText(t, startX+10, startY+40, blue);
             }
         }
 
@@ -105,38 +117,21 @@ public class GridView extends View {
 
             if (i % 3 == 0){
                 canvas.drawLine(startX, startY, stopX, stopY, black);
+                //if (i != 9)
+                //    canvas.drawText(s, startX+10, startY+40, blue);
             } else {
                 canvas.drawLine(startX, startY, stopX, stopY, gray);
+                //canvas.drawText(t, startX+10, startY+40, blue);
             }
         }
 
-//        canvas.drawColor(Color.WHITE);
-//
-//        if (numColumns == 0 || numRows == 0) {
-//            return;
-//        }
-//
-//        int width = getWidth();
-//        int height = getHeight();
-//
-//        for (int i = 0; i < numColumns; i++) {
-//            for (int j = 0; j < numRows; j++) {
-//                if (cellChecked[i][j]) {
-//
-//                    canvas.drawRect(i * cellWidth, j * cellHeight,
-//                            (i + 1) * cellWidth, (j + 1) * cellHeight,
-//                            blackPaint);
-//                }
-//            }
-//        }
-//
-//        for (int i = 1; i < numColumns; i++) {
-//            canvas.drawLine(i * cellWidth, 0, i * cellWidth, height, blackPaint);
-//        }
-//
-//        for (int i = 1; i < numRows; i++) {
-//            canvas.drawLine(0, i * cellHeight, width, i * cellHeight, blackPaint);
-//        }
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (cellChecked[row][col]){
+                    canvas.drawText(s, xOffset+row*gridSpacing, yOffset+col*gridSpacing, blue);
+                }
+            }
+        }
     }
 
     @Override
@@ -144,6 +139,8 @@ public class GridView extends View {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             int column = (int)(event.getX() / cellWidth);
             int row = (int)(event.getY() / cellHeight);
+
+            Log.i("CellSelected", "onTouchEvent: " + column + ", " + row);
 
             cellChecked[column][row] = !cellChecked[column][row];
             invalidate();
