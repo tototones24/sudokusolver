a7c13448683399063ee9385cd504485c92786ba9
Antonio Contreras <antoniocontreras@Antonios-MBP-2.att.net>
2017-09-29 13:17:57 -0700

    Grid is up

--- app/src/main/java/com/example/antoniocontreras/sudokusolver/MainActivity.java
+++ app/src/main/java/com/example/antoniocontreras/sudokusolver/MainActivity.java
@@ -9,5 +9,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //test
+        PixelGridView pixelGrid = new PixelGridView(this);
+        pixelGrid.setNumColumns(9);
+        pixelGrid.setNumRows(9);
+
+        setContentView(pixelGrid);
+
+//        int[][] grid = new int[9][9];
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = 0; j < 9; j++) {
+//                grid[i][j] = 9;
+//            }
+//        }
+//
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = 0; j < 9; j++) {
+//                System.out.print(grid[i][j] + "|");
+//            }
+//        }
+
+
     }
+
+
+
 }
